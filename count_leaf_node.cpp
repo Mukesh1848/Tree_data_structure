388b829f9a1f49f2e1e7622239980eeae987042d
Mukesh Kumar <97693278+Mukesh1848@users.noreply.github.com>
2022-10-14 22:15:26 +0530

    Create count_leaf_node.cpp

---
+++ count_leaf_node.cpp
@@ -0,0 +1,24 @@
+
+void inorder(BinaryTreeNode<int> * root, int &count) {
+    //base case
+    if(root == NULL) {
+        return ;
+    }
+
+    inorder(root->left, count);
+   	
+    //leaf node
+    if(root->left == NULL && root->right == NULL) {
+        count++;
+    }
+    
+    inorder(root->right, count);
+
+}
+
+
+int noOfLeafNodes(BinaryTreeNode<int> *root){
+    int cnt = 0;
+    inorder(root, cnt);
+    return cnt;
+}
